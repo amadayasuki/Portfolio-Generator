eb2398f817c4b7e461a31559c60ff8dee80011c0
Sensess <SensessYentzer@outlook.com>
2022-05-06 15:28:14 -0400

    Modularize Code

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
- 
-  };
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
